52dfd7b0c9e320695178f2a123c548f53da65bcc
Rizwan-955 <123janjeejan@gmail.com>
2026-07-05 09:08:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,52 +5,31 @@ import {
     onValue
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-// Firebase Live Data Reference
-const liveRef = ref(database, "HydroMonitor/Live");
+const liveRef = ref(database, "HydroUnit");
 
-// Listen for changes
 onValue(liveRef, (snapshot) => {
 
     const data = snapshot.val();
 
-    if (!data) {
-        console.log("No data found.");
+    if (!data)
         return;
-    }
 
-    // Voltage
     document.getElementById("voltage").innerHTML =
         Number(data.Voltage || 0).toFixed(2) + " V";
 
-    // Current
     document.getElementById("current").innerHTML =
         Number(data.Current || 0).toFixed(2) + " A";
 
-    // Power
     document.getElementById("power").innerHTML =
         Number(data.Power || 0).toFixed(2) + " W";
 
-    // Energy
     document.getElementById("energy").innerHTML =
-        Number(data.Energy_kWh || 0).toFixed(3) + " kWh";
+        Number(data.Energy || 0).toFixed(3) + " Wh";
 
-    // Last Update
+    document.getElementById("status").innerHTML = "🟢 ONLINE";
+    document.getElementById("status").style.color = "green";
+
     document.getElementById("lastUpdate").innerHTML =
-        data.LastUpdate || "--";
-
-    // Status
-    const status = document.getElementById("status");
-
-    if (data.Status === "Online") {
-
-        status.innerHTML = "🟢 ONLINE";
-        status.style.color = "green";
-
-    } else {
-
-        status.innerHTML = "🔴 OFFLINE";
-        status.style.color = "red";
-
-    }
+        new Date().toLocaleTimeString();
 
 });
