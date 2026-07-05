49b28401ff8a2970febdc455a03e445f95f3797c
Rizwan-955 <123janjeejan@gmail.com>
2026-07-05 11:09:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let offlineTimer;
 
 import {
     ref,
-    onValue
+    onValue,
+    get
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const liveRef = ref(database, "HydroUnit");
@@ -67,3 +68,96 @@ onValue(liveRef, (snapshot) => {
     },5000);
 
 });
+//=====================================
+// HISTORY BUTTON
+//=====================================
+
+const historyBtn =
+document.getElementById("historyBtn");
+
+const historySection =
+document.getElementById("historySection");
+
+let opened = false;
+
+historyBtn.onclick = function(){
+
+    opened = !opened;
+
+    if(opened)
+    {
+
+        historySection.style.display="block";
+
+        historyBtn.innerHTML="▲ Hide History";
+
+        loadHistory();
+
+    }
+    else
+    {
+
+        historySection.style.display="none";
+
+        historyBtn.innerHTML="▼ View History";
+
+    }
+
+};
+
+//=====================================
+// LOAD HISTORY
+//=====================================
+
+async function loadHistory()
+{
+
+    const today = new Date();
+
+    const year = today.getFullYear();
+
+    const month =
+    String(today.getMonth()+1).padStart(2,'0');
+
+    const day =
+    String(today.getDate()).padStart(2,'0');
+
+    const date =
+    year+"-"+month+"-"+day;
+
+    const historyRef =
+    ref(database,"History/"+date);
+
+    const snapshot =
+    await get(historyRef);
+
+    if(!snapshot.exists())
+        return;
+
+    const history =
+    snapshot.val();
+
+    for(let hour in history)
+    {
+
+        const item = history[hour];
+
+        document.getElementById(
+        "h"+hour+"v").innerHTML =
+        Number(item.Voltage).toFixed(2)+" V";
+
+        document.getElementById(
+        "h"+hour+"c").innerHTML =
+        Number(item.Current).toFixed(2)+" A";
+
+        document.getElementById(
+        "h"+hour+"p").innerHTML =
+        Number(item.Power).toFixed(2)+" W";
+
+        document.getElementById(
+        "h"+hour+"e").innerHTML =
+        Number(item.Energy).toFixed(3)+" Wh";
+
+    }
+
+}
